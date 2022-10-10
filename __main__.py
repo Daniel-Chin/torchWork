1a923300e404f1739c8af5b0af2378a2170a117a
Daniel Chin <daniel.chin@nyu.edu>
2022-10-10 19:43:49 +0400

    CLI for loss preview

--- __main__.py
+++ __main__.py
@@ -1,3 +1,15 @@
-import torchWork.demo
+import argparse
 
-torchWork.demo.main()
+import torchWork.demo
+import torchWork.loss_logger
+
+parser = argparse.ArgumentParser()
+parser.add_argument(
+    '--preview-loss', dest='preview_loss', 
+)
+args = parser.parse_args()
+
+if args.preview_loss is not None:
+    torchWork.loss_logger.previewLosses(args.preview_loss)
+else:
+    torchWork.demo.main()
