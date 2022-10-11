5b720c35468282a98c440c28a097e450c477946d
Daniel Chin <daniel.chin@nyu.edu>
2022-10-11 10:19:22 +0400

    fix import

--- __init__.py
+++ __init__.py
@@ -1,10 +1,10 @@
+from torchWork.device import *
 from torchWork.loss_weight_tree import LossWeightTree
 import torchWork.loss_tree as loss_tree
 LossTree = loss_tree.LossTree
 from torchWork.loss_logger import LossLogger
 from torchWork.hyper_params import BaseHyperParams
 from torchWork.profiler import Profiler
-from torchWork.device import *
 from torchWork.experiment_control import (
     ExperimentGroup, runExperiment, 
 )
