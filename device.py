48f3abf2df2860ecd58195966c161ace8a4b5ed5
Daniel Chin <daniel.chin@nyu.edu>
2022-10-11 09:10:59 +0400

    flush print

--- device.py
+++ device.py
@@ -5,7 +5,7 @@ CUDA = torch.device("cuda:0")
 CPU  = torch.device("cpu")
 if HAS_CUDA:
     DEVICE = CUDA
-    print('We have CUDA.')
+    print('We have CUDA.', flush=True)
     # gpu_name = torch.cuda.get_device_name(
     #     torch.cuda.current_device(), 
     # )
